4ae59b00e51e35aacc3c662e5a8380ee453afd10
Jesse Dovi <jdovi@drydenengineering.com>
2014-02-04 12:56:49 -0500

    got the markup app working

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_DIRS = BASE_DIR + '\\templates\\'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -39,6 +40,7 @@ INSTALLED_APPS = (
 	'django.contrib.admin',
 	'myapp',
 	'south',
+	'markdown_deux',
 )
 
 MIDDLEWARE_CLASSES = (
